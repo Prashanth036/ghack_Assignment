a789b0815e56381e8e4954214e0f969a77f60688
prashanth <prashanthh036@gmail.com>
2024-10-18 02:09:41 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ghack_assignment/",
+  base: "/ghack_Assignment/",
   define: {
     'process.env': process.env 
   },
